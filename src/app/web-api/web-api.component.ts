5ffa54bfd3c6edc81a920529f2f3c963a14ec4a0
rwxproject <rwxproject@gmail.com>
2021-11-27 14:07:51 -0500

    update web-app

--- src/app/web-api/web-api.component.ts
+++ src/app/web-api/web-api.component.ts
@@ -17,35 +17,40 @@ export class WebApiComponent implements OnInit {
   time = '';
   userId = '';
   user = '';
+  commonHost = '';
+  commonTime = '';
 
   constructor(private webapiService: WebapiService) {
-    this.webapiService.getWebapi('user1').subscribe((response: WebApi) => {
+    this.fetchApi(this.user);
+  }
+
+  getApi() {
+    this.fetchApi(this.user);
+  }
+
+  getApiCommon() {
+    console.log(`request common service`);
+    this.webapiService.getCommonWebapi().subscribe((response: WebApi) => {
+      this.commonHost = response.host;
+      this.commonTime = response.time;
+    });
+  }
+
+  fetchApi(user: string) {
+    console.log(`request for user: ${user}`);
+    this.webapiService.getWebapi(user).subscribe((response: WebApi) => {
       this.host = response.host;
       this.time = response.time;
       this.userId = response.userId;
     });
   }
+
   onInput(value: string) {
     console.log(value);
     this.user = value;
   }
 
-  onClickMe() {
-    console.log(`request for user: ${this.user}`);
-    this.webapiService.getWebapi(this.user).subscribe((response: WebApi) => {
-      this.host = response.host;
-      this.time = response.time;
-      this.userId = response.userId;
-    });
+  ngOnInit(): void {
+    this.user = 'user1';
   }
-
-  onClickMeCommon() {
-    console.log(`request common`);
-    this.webapiService.getCommonWebapi().subscribe((response: WebApi) => {
-      this.host = response.host;
-      this.time = response.time;
-    });
-  }
-
-  ngOnInit(): void {}
 }
