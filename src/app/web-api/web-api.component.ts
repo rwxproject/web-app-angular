a259b4b057f5efd5dbef401e0f32cfa38bba6c45
rwxproject <rwxproject@gmail.com>
2021-11-27 12:11:49 -0500

    web-app update

--- src/app/web-api/web-api.component.ts
+++ src/app/web-api/web-api.component.ts
@@ -1,4 +1,3 @@
-import { NodeWithI18n } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { WebapiService } from '../webapi.service';
 
@@ -17,9 +16,23 @@ export class WebApiComponent implements OnInit {
   host = '';
   time = '';
   userId = '';
+  user = '';
 
   constructor(private webapiService: WebapiService) {
-    this.webapiService.getWebapi().subscribe((response: WebApi) => {
+    this.webapiService.getWebapi('user1').subscribe((response: WebApi) => {
+      this.host = response.host;
+      this.time = response.time;
+      this.userId = response.userId;
+    });
+  }
+  onInput(value: string) {
+    console.log(value);
+    this.user = value;
+  }
+
+  onClickMe() {
+    console.log(`request fou user: ${this.user}`);
+    this.webapiService.getWebapi(this.user).subscribe((response: WebApi) => {
       this.host = response.host;
       this.time = response.time;
       this.userId = response.userId;
--- src/app/webapi.service.ts
+++ src/app/webapi.service.ts
@@ -13,8 +13,9 @@ export interface WebapiResponse {
 export class WebapiService {
   constructor(private http: HttpClient) {}
 
-  getWebapi() {
-    const headers = new HttpHeaders().set('x-user-id', 'user1');
+  getWebapi(user: string) {
+    console.log(`service request user: ${user}`);
+    const headers = new HttpHeaders().set('x-user-id', user);
     return this.http.get<WebapiResponse>('http://system1.app.rwx.systems/api', {
       headers: headers,
     });
